9606b672b6b0e905356226b96f2da42971765ae7
Анастасія <opanasenko_ak23@nuwm.edu.ua>
2025-02-12 19:41:52 +0200

    Adding new files to the repo.

--- gcd.py
+++ gcd.py
@@ -9,3 +9,15 @@ def gcd(n, m):
 
 
 print(gcd(34, 56))
+
+
+def test_gcd(n, m):
+    d = gcd(n, m)
+
+    assert d > 0  # 1) `d` is positive
+    assert n % d == 0  # 2) `d` divides `n`
+    assert m % d == 0  # 3) `d` divides `m`
+
+    # 4) no other number larger than `d` divides both `n` and `m`
+    for i in range(d + 1, min(n, m)):
+        assert (n % i) or (m % i)
